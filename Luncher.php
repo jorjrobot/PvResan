a523fe25619e883d42045c102d2935af2b22e20b
TeleGold <omid.http@gmail.com>
2016-08-06 20:21:09 +0430

    هر گونه کپی برداری حرام است و من راضی نیستم

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '212829017:AAGDVKVQ7JCT2dEcH-HrX98xSzwei2Gl2Ng');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = 194849320;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
